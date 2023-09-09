69190f226fcbb2affe9f1a50e0390a81ff9eb170
Khusan <khusanabdukarimov007@gmail.com>
2023-09-09 18:57:58 +0500

    add configuration

---
+++ src/config/configOptions.ts
@@ -0,0 +1,19 @@
+type HttpOptions = {
+    port: number;
+}
+type ApplicationOptions = {
+    http: HttpOptions;
+}
+
+type MongoDBOptions = {
+    user: string;
+    password: string;
+    host: string;
+    port: number;
+    database: string;
+}
+
+export type ConfigurationOptions = {
+    application: ApplicationOptions;
+    mongodb: MongoDBOptions;
+}
---
+++ src/config/index.ts
@@ -0,0 +1,27 @@
+import dotEnv from "dotenv";
+import { ConfigurationOptions } from "./configOptions";
+import * as process from "process";
+
+
+if (process.env.NODE_ENV === "TEST") {
+    dotEnv.config({ path: '../../.env.test'});
+} else {
+    dotEnv.config();
+}
+
+const configuration: ConfigurationOptions = {
+    application: {
+        http: {
+            port: Number( process.env.HTTP_PORT) || 4000
+        }
+    },
+    mongodb: {
+        user: process.env.MONGODB_USER,
+        password: process.env.MONGODB_PASSWORD,
+        host: process.env.MONGODB_HOST || 'localhost',
+        port: Number(process.env.MONGODB_PORT) || 27017,
+        database: process.env.MONGODB_DATABASE
+    }
+}
+
+export default configuration;
