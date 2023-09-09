6142fc05bab9cfbbd721f0f91ed97ea1ecd705ff
Khusan <khusanabdukarimov007@gmail.com>
2023-09-09 21:24:39 +0500

    feat: add new changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app.module';
+import { NestFactory } from "@nestjs/core";
+
+import { AppModule } from "./modules/app.module";
+import swagger from "./common/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	void swagger(app);
+
+	await app.listen(3000);
 }
-bootstrap();
+
+void bootstrap();
